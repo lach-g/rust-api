d13a21ea250e7363eafee001a6dc70b9a1196313
lach-g <lachlan.greenland@gmail.com>
2024-02-28 19:42:20 +0800

    add logging and env variable in task def to specify info level

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,14 @@
-use chrono;
-use axum::{
-    routing::get,
-    Router,
-};
+use axum::{routing::get, Router};
+use env_logger;
+use log::info;
 
 #[tokio::main]
-async fn main()
-{
+async fn main() {
+    env_logger::init();
+    let address = "0.0.0.0:3000";
+
+    info!("Configuring to run on {address}");
+
     // build our application with a single route
     let app = Router::new()
         .route("/hello", get(handle_hello))
@@ -14,21 +16,20 @@ async fn main()
         .route("/time", get(handle_time));
 
     // run our app with hyper, listening globally on port 3000
-    let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
+    let listener = tokio::net::TcpListener::bind(&address).await.unwrap();
+    info!("Serving on {address}");
     axum::serve(listener, app).await.unwrap();
 }
 
-async fn handle_hello() -> &'static str
-{
+async fn handle_hello() -> &'static str {
     "Hello there"
 }
 
-async fn handle_about() -> &'static str
-{
+async fn handle_about() -> &'static str {
     "This is a server written in Rust!"
 }
-
 
 async fn handle_time() -> String {
     format!("Current Local Time: {:?}", chrono::offset::Local::now())
 }
+
